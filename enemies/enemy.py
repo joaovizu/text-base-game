2f6d42a6ff022a6af6479ac0337ebec9d6bc1f27
JaoVizu <joaovizu2015@gmail.com>
2026-05-18 20:52:20 -0300

    added critic attack

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,3 +1,12 @@
+from logging import critical
+from random import randint
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from hero.hero import Hero
+from ui_helper import UI
+
+
 class Enemy:
     def __init__(self, name: str, hp: int, damage: int, xp_drop: int):
         self.damage = damage
@@ -8,8 +17,17 @@ class Enemy:
     def appear(self):
         print(f'A wild {self.name.upper()} has appeared!')
 
-    def attack(self):
-        print(f'{self.name} attacks for {self.damage} damage!')
+    def attack(self, hero: Hero) -> None:
+        self.critic_attack(hero)
+        hero.hp -= self.damage
+        UI.announce(f'{self.name} attacks for {self.damage} damage! Your HP is now {hero.hp}', UI.ERROR)
+
+    def critic_attack(self, hero: Hero) -> None:
+        if randint(1, 10) == 2:
+            hero.hp = self.damage * 2
+            UI.announce(f'{self.name} CRITIC ATTACK for {self.damage * 2} damage! Your HP is now {hero.hp}', UI.ERROR)
+            return
+
 
     def is_alive(self):
         return self.hp > 0
--- enemies/spider.py
+++ enemies/spider.py
@@ -1,16 +1,9 @@
 from random  import randint
 
 from enemies.enemy import Enemy
-import hero.hero as Hero
-from ui_helper import UI
-
 
 class Spider(Enemy):
 
     def __init__(self):
         #Using super to fill the parents class requirements
         super().__init__(name='Spider', hp=10, damage=3, xp_drop=randint(1, 10))
-
-    def attack(self, hero: Hero):
-        hero.hp -= self.damage
-        UI.announce(f'{self.name.upper()} attacked you, your HP is now {hero.hp}', UI.ERROR)
