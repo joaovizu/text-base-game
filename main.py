19be9ee78af1b249e2e436c42422fbc5d7b4272f
JaoVizu <joaovizu2015@gmail.com>
2026-05-18 20:23:16 -0300

    fixed main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # THE MAIN SCRIPT, HERE WE START AND END GAME
 import config
 import database.game_db as db
-from game import game
+from game import GameManager
 
 db.init_db()
 
 while True:
     if config.IS_GAME_OVER: break
     else:
-        game()
+        game = GameManager()
+        game.run()
